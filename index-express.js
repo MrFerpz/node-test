f26550149b263349024829632279007bf1187134
Fred <leferpz@gmail.com>
2025-03-20 16:15:10 -0400

    Fixed send command for contact me link

--- index-express.js
+++ index-express.js
@@ -4,7 +4,7 @@ const app = express();
 app.get("/", (req, res) => res.send("Hello world!"))
 app.get("/index.html", (req, res) => res.sendFile("/Users/fredpy/repos/basic-node-site/index.html"))
 app.get("/about.html", (req, res) => res.sendFile("/Users/fredpy/repos/basic-node-site/about.html"))
-app.get("/contact-me.html", (req, res) => res.send("/Users/fredpy/repos/basic-node-site/contact-me.html"))
+app.get("/contact-me.html", (req, res) => res.sendFile("/Users/fredpy/repos/basic-node-site/contact-me.html"))
 
 const PORT = 3050;
 app.listen(PORT, () => {
