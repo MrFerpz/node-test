3155bc3ff3450b52053c2c5d620a11961e099ab2
Fred <leferpz@gmail.com>
2025-03-21 11:40:35 -0400

    Made a controller and database, separated functions from the routing files

--- routes/authorRouter.js
+++ routes/authorRouter.js
@@ -1,10 +1,15 @@
 const { Router } = require('express');
+const { getAuthorById } = require('../controllers/authorController')
 const authorRouter = Router();
 
 authorRouter.get("/", (req, res) => res.send("Welcome to the author homepage."));
-authorRouter.get("/:authorId", (req, res) => {
-    const {authorId} = req.params;
-    res.send(`Author ID: ${authorId}`);
-});
+authorRouter.get("/:authorId", getAuthorById)
+// getAuthorById from the controller handles the logic and replaces the prior method we defined
+
+    // (req, res) => {
+    // console.log(req.params);
+    // const {authorId} = req.params;
+    // res.send(`Author ID: ${authorId}`);
+;
 
 module.exports = authorRouter;
